68e83475a19c37ceb6ba3808da9deef9c132d121
tkrisztián <tkrisztin@mitiszkad.lan>
2023-09-14 09:07:35 +0200

    First

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static Dtos.Dtos;
+
+namespace Dtos.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static readonly List<ProductDto> productDtos = new()
+        {
+            new ProductDto(Guid.NewGuid(), "Termék1", 2500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(), "Termék2", 5500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(), "Termék3", 12500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+        };
+    }
+}
---
+++ Dtos.cs
@@ -0,0 +1,8 @@
+﻿namespace Dtos
+{
+    public class Dtos
+    {
+        public record ProductDto(Guid Id, string ProductName, int ProductPrice, DateTimeOffset CreatedTime,
+            DateTimeOffset Modified);
+    }
+}
